963c0ee3da674da512ee03ae392019ca6eece837
Shailesh <garg.shailesh@gmail.com>
2014-05-01 18:25:05 -0700

    initial version

---
+++ README.md
@@ -0,0 +1,6 @@
+mvn compile
+mvn package
+target\bin\webapp.bat
+
+http://localhost:8080/
+
---
+++ src/main/webapp/js/controllers.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/* Controllers */
+
+app.controller("Page1Controller", function ($scope, $routeParams, $http) {
+	$scope.data = "Hello World";
+});
+
+app.controller("Page2Controller", function ($scope, $routeParams, $http) {
+	$scope.data = "Hello World";
+});

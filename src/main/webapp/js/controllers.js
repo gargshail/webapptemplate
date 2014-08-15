743ddec0fe862c67ce7a75a57efdbb085ce03222
Shailesh <garg.shailesh@gmail.com>
2014-08-14 19:44:18 -0700

    added useFileMappedBuffer to false, changed pom.xml with overrideDescriptor

--- src/main/webapp/js/controllers.js
+++ src/main/webapp/js/controllers.js
@@ -6,6 +6,9 @@ app.controller("Page1Controller", function ($scope, $routeParams, $http) {
 	$scope.data = "Hello World";
 });
 
+
 app.controller("Page2Controller", function ($scope, $routeParams, $http) {
 	$scope.data = "Hello World";
 });
+
+
